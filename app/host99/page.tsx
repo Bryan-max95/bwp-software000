ad3bcc9f27331b5b88dd1c432e9df88d284d4c6f
Bryan-max95 <datasendnetworkbryan@gmail.com>
2026-07-20 20:54:51 -0500

    Update page.tsx

--- app/host99/page.tsx
+++ app/host99/page.tsx
@@ -3,9 +3,13 @@ import AdminPage from "../admin/page";
 
 export const metadata: Metadata = {
   title: "Portal de Gestión | BWP Software",
-  robots: { index: false, follow: false, nocache: true }
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+  },
 };
 
-export default function HostPortalPage(){
+export default function HostPortalPage() {
   return <AdminPage />;
 }
